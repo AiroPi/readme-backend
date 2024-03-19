f3dedc964b6a0671987bdf1f6576be40cec889e4
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2024-03-20 00:18:18 +0100

    fix Last-Modified headers

--- src/routers/minesweeper_router.py
+++ src/routers/minesweeper_router.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from pathlib import Path
 
 import minesweeper
@@ -14,23 +15,30 @@ game = minesweeper.Minesweeper((14, 16), 35)
 flag_mode = False
 
 
+def get_header():
+    return {
+        "Cache-Control": "no-cache, max-age=0",
+        "Last-Modified": datetime.now().strftime("%a, %d %b %Y %H:%M:%S %Z"),
+    }
+
+
 @router.get("/img/{i}/{j}")
 def get_board_img(i: int, j: int):
     if (i, j) in game.revealed:
         if game.board[i][j] == -1:
             return FileResponse(
                 BASE_PATH / "mine_red.png",
-                headers={"Cache-Control": "no-cache, max-age=0"},
+                headers=get_header(),
             )
         return FileResponse(
             BASE_PATH / f"{game.board[i][j]}.png",
-            headers={"Cache-Control": "no-cache, max-age=0"},
+            headers=get_header(),
         )
     if game.game_over and game.board[i][j] == -1:
-        return FileResponse(BASE_PATH / "mine.png", headers={"Cache-Control": "no-cache, max-age=0"})
+        return FileResponse(BASE_PATH / "mine.png", headers=get_header())
     if (i, j) in game.flags:
-        return FileResponse(BASE_PATH / "flag.png", headers={"Cache-Control": "no-cache, max-age=0"})
-    return FileResponse(BASE_PATH / "closed.png", headers={"Cache-Control": "no-cache, max-age=0"})
+        return FileResponse(BASE_PATH / "flag.png", headers=get_header())
+    return FileResponse(BASE_PATH / "closed.png", headers=get_header())
 
 
 @router.get("/img/{text}")
@@ -38,26 +46,24 @@ def get_img(text: str):
     match text:
         case "flag-toggle":
             if flag_mode:
-                return FileResponse(BASE_PATH / "flag.png", headers={"Cache-Control": "no-cache, max-age=0"})
+                return FileResponse(BASE_PATH / "flag.png", headers=get_header())
             else:
-                return FileResponse(
-                    BASE_PATH / "deactivated_flag.png", headers={"Cache-Control": "no-cache, max-age=0"}
-                )
+                return FileResponse(BASE_PATH / "deactivated_flag.png", headers=get_header())
         case "digit1":
-            return FileResponse(BASE_PATH / "dnull.png", headers={"Cache-Control": "no-cache, max-age=0"})
+            return FileResponse(BASE_PATH / "dnull.png", headers=get_header())
         case "digit2":
-            return FileResponse(BASE_PATH / "d3.png", headers={"Cache-Control": "no-cache, max-age=0"})
+            return FileResponse(BASE_PATH / "d3.png", headers=get_header())
         case "digit3":
-            return FileResponse(BASE_PATH / "d5.png", headers={"Cache-Control": "no-cache, max-age=0"})
+            return FileResponse(BASE_PATH / "d5.png", headers=get_header())
         case "header":
-            return FileResponse(BASE_PATH / "header.png", headers={"Cache-Control": "no-cache, max-age=0"})
+            return FileResponse(BASE_PATH / "header.png", headers=get_header())
         case "undo":
-            return FileResponse(BASE_PATH / "undo.png", headers={"Cache-Control": "no-cache, max-age=0"})
+            return FileResponse(BASE_PATH / "undo.png", headers=get_header())
         case "face":
             if game.game_over:
-                return FileResponse(BASE_PATH / "face_lose.png", headers={"Cache-Control": "no-cache, max-age=0"})
+                return FileResponse(BASE_PATH / "face_lose.png", headers=get_header())
             else:
-                return FileResponse(BASE_PATH / "face.png", headers={"Cache-Control": "no-cache, max-age=0"})
+                return FileResponse(BASE_PATH / "face.png", headers=get_header())
         case _:
             return 404
 
